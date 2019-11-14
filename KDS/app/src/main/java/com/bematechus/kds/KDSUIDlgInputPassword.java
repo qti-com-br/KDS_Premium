fc61e2d80f154cf1a09dd76e5cca717c75862463
bema-davidwong <42630058+bema-davidwong@users.noreply.github.com>
2019-11-14 16:55:49 +0800

    backup

--- KDS/app/src/main/java/com/bematechus/kds/KDSUIDlgInputPassword.java
+++ KDS/app/src/main/java/com/bematechus/kds/KDSUIDlgInputPassword.java
@@ -170,6 +170,8 @@ public class KDSUIDlgInputPassword  extends KDSUIDialogBase {
      */
     protected boolean checkDataValidation()
     {
+        if ( this.getView().findViewById(R.id.linearConfirm).getVisibility() != View.VISIBLE)
+            return true;
         String strConfirm =  m_txtConfirm.getText().toString();
         String strPwd =m_txtText.getText().toString();
         boolean b = strConfirm.equals(strPwd);
