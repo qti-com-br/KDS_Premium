dd9640d1cdb4c29915a6009956e16c4674381ebe
bema-davidwong <42630058+bema-davidwong@users.noreply.github.com>
2020-06-15 16:12:41 +0800

    use 9999 as default station id.

--- KDSLib/kdslib/src/main/java/com/bematechus/kdslib/Activation.java
+++ KDSLib/kdslib/src/main/java/com/bematechus/kdslib/Activation.java
@@ -2281,7 +2281,7 @@ public class Activation implements ActivationHttp.HttpEvent , Runnable {
         if (m_receiver != null)
             m_receiver.onSyncWebReturnResult(ActivationRequest.COMMAND.Cleaning, str, SyncDataResult.OK);
     }
-
+    public static final int NEW_STATION_ID = 9999;
     /**
      * check if this station has been registered
      * kpp1-340
@@ -2292,6 +2292,10 @@ public class Activation implements ActivationHttp.HttpEvent , Runnable {
         StoreDevice dev =  findMyLicense();
         if (dev == null)
             return "";
+        String id = dev.getID();
+        if (id.equals(KDSUtil.convertIntToString(NEW_STATION_ID)))
+            return "";
+
         return dev.getID();
     }
 }
